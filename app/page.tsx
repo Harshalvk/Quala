5e57626c676f1b898152687e6ea0b4d90e462d7f
Harshalvk <harshalvkhobragade@gmail.com>
2025-06-26 19:41:10 +0530

    refactor: restructure Home component layout and simplify markup

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,15 @@
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
+import Bento from "./_components/Bento";
+import Footer from "./_components/Footer";
+import Hero from "./_components/Hero";
+import Navbar from "./_components/Navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl font-semibold">Quala</h1>
-      <div className="flex flex-col gap-4">
-        <Link
-          href={"/sign-in"}
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          Sign In
-        </Link>
-        <Link
-          href={"/dashboard"}
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          Dashboard
-        </Link>
-        <Link
-          href={"/pricing"}
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          Pricing
-        </Link>
-      </div>
-    </div>
+    <section className="max-w-5xl mx-auto p-3">
+      <Navbar />
+      <Hero />
+      <Bento />
+      <Footer />
+    </section>
   );
 }
